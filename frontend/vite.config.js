9c81aeca1ddeab96ae12569d0f583273498cead2
sravan_yandapalli <yandapallisravankumar@gmail.com>
2024-10-03 00:38:39 +0530

    hdh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    base: '/',
     outDir: 'dist', // Ensure build output goes to 'dist' directory
   },
 });
